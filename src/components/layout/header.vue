28c5f522f9ebe94ca793fe57516799d3fedb50ad
8Liu <2969878315@qq.com>
2019-12-10 23:36:59 +0800

    Initial commit

---
+++ src/components/layout/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-header>
+    <div class="collapse-btn">
+      <i class="el-icon-s-operation"></i>
+    </div>
+    <div class="dropdown">
+      <el-dropdown>
+        <i class="el-icon-setting"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </div>
+
+  </el-header>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style>
+  .el-header{
+    display: flex;
+    justify-content: space-between;
+    height: 56px;
+    width: 100%;
+    text-align: right;
+    background-color: #409EFF;
+    color: #303133;
+    line-height: 60px;
+  }
+</style>
+
---
+++ src/components/layout/home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main">
+    <v-aside></v-aside>
+    <div class="main-body" :collapse="true">
+      <v-header></v-header>
+      <div class="main-container" :collapse="true">
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    import vAside from '@/components/layout/aside'
+    import vHeader from '@/components/layout/header'
+
+    export default {
+        name: "home",
+        components: {
+            vAside,
+            vHeader
+        }
+
+    }
+</script>
+
+<style scoped>
+  .main {
+    display: flex;
+    flex-wrap: nowrap;
+    width: 100%;
+    height: 100%;
+  }
+
+
+
+  .main .main-body {
+    order: 1;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+
+  .main-body v-header {
+    height: 56px;
+    width: 100%;
+    text-align: right;
+    background-color: #409EFF;
+    color: #303133;
+    line-height: 60px;
+  }
+
+</style>
